d3cc57c65b4fadd621745c109d4a4fe1b78251c1
lvxc <coderlxc@gmail.com>
2025-01-06 15:40:42 +0800

    init

---
+++ api/src/test/java/com/lvxianchao/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvxianchao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
